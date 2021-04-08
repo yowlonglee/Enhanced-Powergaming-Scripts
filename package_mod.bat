81196631be006fc0335ee304dbd4adeaae5f0c26
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2021-04-07 21:57:11 -0500

    Update Paladin Script
    Paladin script now covers all paladin kits instead of just inquisitors.

--- package_mod.bat
+++ package_mod.bat
@@ -24,7 +24,7 @@ set audio_folder=%mod_folder%\audio
 set tispack_folder=%mod_folder%\tiz
 set tile2ee_folder=%mod_folder%\tools\tile2ee
 set iconv_folder=%mod_folder%\languages\iconv
-set mod_readme=http://www.gibberlings3.net/readmes/readme-%mod_folder%.html
+set mod_readme=https://github.com/Gibberlings3/Enhanced-Powergaming-Scripts/blob/main/README.md
 
 REM /* MODIFY: if you don't need to build a specific package, you can disable it by setting the appropriate variable below to 0 */
 set build_windows=1
