a4a1a0b60fce9036150ea38f589320e82ebbc6c4
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-03-22 10:17:39 -0700

    Check Cooldown

--- enhanced-powergaming-scripts/lua/m_actri3.lua
+++ enhanced-powergaming-scripts/lua/m_actri3.lua
@@ -1,3 +1,3 @@
-function MO_AmCastingCooldown(sprite)
-    return sprite.getCastTimerPercentage() > 0
+function MO_AmCastingCooldown()
+    return EEex_LuaTrigger_Object:getCastTimerPercentage() > 0
 end
