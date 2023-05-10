0bc637c8a35d6cd6320eafd995e106dbc09e5172
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-05-10 08:08:14 -0700

    IWD2: EE Scripts

---
+++ enhanced-powergaming-scripts/lua/m_actri4.lua
@@ -0,0 +1,36 @@
+function AmAttacking(actorID)
+    return ( { [3] = true, [105] = true, [134] = true } )[IEex_GetActorCurrentAction(actorID)]
+end
+
+--function IsAttacking(object, trigger)
+--    if trigger.m_triggerID == 0x410F then
+--        local actorID = EEex_EvalObjectAsActor(EEex_ParseObjectString(object), EEex_GetActorIDSelected())
+--        local actionID = EEex_GetActorCurrentAction(actorID)
+--        if actionID == 3 or actionID == 105 or actionID == 134 then
+--            return true
+
+--        else
+--            return false
+--        end
+--    else
+--        return false
+--    end
+--end
+--EEex_InstallNewTriggers(IsAttacking)
+
+--function IsAttacking(object)
+    --local actorID = EEex_EvalObjectAsActor(EEex_ParseObjectString(object), EEex_GetActorIDSelected())
+    --local actionID = EEex_GetActorCurrentAction(actorID)
+    --object.m_id
+    --local actionID = EEex_GetActorCurrentAction(object.m_actionID)
+    --if actionID == 3 or actionID == 105 or actionID == 134 then
+--    if object.m_actionID == 3 or object.m_actionID == 105 or object.m_actionID == 134 then 
+--        EEex_LuaTrigger = true
+--        return
+--    end
+--    EEex_LuaTrigger = false
+--end
+
+-- local actionID = EEex_GetActorCurrentAction(actorID)
+-- object.m_id
+-- object:isSprite()
---
+++ enhanced-powergaming-scripts/lua/m_actri5.lua
@@ -0,0 +1,3 @@
+function AmMoving()
+    return ( { [22] = true, [23] = true } )[IEex_GetActorCurrentAction(actorID)]
+end
