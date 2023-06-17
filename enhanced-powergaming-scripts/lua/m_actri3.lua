7d9f23e166fd02647bd3f9e0284df0956b53dc38
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-06-16 23:11:55 -0700

    update lua checks

--- enhanced-powergaming-scripts/lua/m_actri3.lua
+++ enhanced-powergaming-scripts/lua/m_actri3.lua
@@ -1,3 +1,11 @@
 function MO_AmCastingCooldown()
-    return EEex_LuaTrigger_Object:getCastTimerPercentage() > 0
+    -- return EEex_LuaTrigger_Object:getCastTimerPercentage() > 0
+    if EEex_LuaTrigger_Object:getCastTimerPercentage() > 0 then
+        if EEex_LuaTrigger_Object:getClass() == 5 and EEex_LuaTrigger_Object:getLocalInt("BDAI_SKILL_MODE") == 3 then
+            EEex_LuaTrigger_Object:setLocalInt("MO_SpellCastTimer", EngineGlobals.g_pBaldurChitin.m_pObjectGame.m_worldTime.m_gameTime + 12 * 15)
+        else
+            EEex_LuaTrigger_Object:setLocalInt("MO_SpellCastTimer", EngineGlobals.g_pBaldurChitin.m_pObjectGame.m_worldTime.m_gameTime + 6 * 15)
+        end
+    end
+    return false
 end
